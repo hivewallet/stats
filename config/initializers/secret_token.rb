bf1a5a46055ffd9189666cd6f50e37e6c63a33f4
Wei Lu <luwei.here@gmail.com>
2014-03-21 13:47:30 +0800

    rails bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HiveStats::Application.config.secret_key_base = 'e09a18232a08ad6401a35ebc76b9aff3722db6faa82aca1e09c3e28a49deeab43a04f37f264be659eca2835282691814c53fa28168e1239fcb19f000e6a63dfe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HiveStats::Application.config.session_store :cookie_store, key: '_hive-stats_session'
